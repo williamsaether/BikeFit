7c3d473bc9524dc0c72a3367ac5b86b5e49f307e
Dobzicle <669810@stud.hvl.no>
2025-04-10 18:17:33 +0200

    Add data augmentation to data generator

--- data/H5DataGenerator.py
+++ data/H5DataGenerator.py
@@ -1,12 +1,15 @@
+import random
 import h5py
 import numpy as np
 import keras.utils
 
 # PyDataset is the new name https://www.tensorflow.org/api_docs/python/tf/keras/utils/PyDataset
 class H5DataGenerator(keras.utils.Sequence):
-    def __init__(self, h5_path, batch_size=32):
+    def __init__(self, h5_path, batch_size=32, data_augmentation=False, flip_indices=None):
         self.h5_path = h5_path
         self.batch_size = batch_size
+        self.data_augmentation = data_augmentation
+        self.flip_indices = flip_indices
 
         with h5py.File(self.h5_path, 'r') as f:
             self.dataset_length = f['images'].shape[0]
@@ -22,5 +25,16 @@ class H5DataGenerator(keras.utils.Sequence):
         with h5py.File(self.h5_path, 'r') as f:
             batch_images = f['images'][batch_indices] / 255.0
             batch_heatmaps = f['heatmaps'][batch_indices] / 255.0
+
+        if self.data_augmentation:
+            for i in range(len(batch_indices)):
+                if random.random() < 0.5:
+                    batch_images[i] = np.fliplr(batch_images[i])
+                    batch_heatmaps[i] = np.fliplr(batch_heatmaps[i])
+
+                    flipped_heatmap = np.zeros_like(batch_heatmaps[i])
+                    for j, flipped_j in enumerate(self.flip_indices):
+                        flipped_heatmap[:, :, flipped_j] = batch_heatmaps[i][:, :, j]
+                    batch_heatmaps[i] = flipped_heatmap
 
         return batch_images, batch_heatmaps
